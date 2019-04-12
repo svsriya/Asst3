cd93ba60918599071940f5454d472230a98633bc
Mahwish Jawed <mj591@cd.cs.rutgers.edu>
2019-04-12 15:52:11 -0400

    server backbone setup

--- WTFserver.c
+++ WTFserver.c
@@ -3,7 +3,7 @@
 #include <pthread.h>
 #include <errno.h>
 #include <sys/types.h>
-#include <sys/sockets.h>
+#include <sys/socket.h>
 #include <netdb.h>
 
 #define ANSI_COLOR_RED     "\x1b[31m"
@@ -23,8 +23,48 @@
  *  5. call to accept (loop to accept multiple connections
 */
 
-int main( int argc, char** argv )
-{
+int main( int argc, char** argv ){
 	
+//	int port_num = argv[1];
+
+	struct addrinfo server;
+	struct addrinfo * results;
+	server.ai_family = AF_INET;
+	server.ai_socktype = SOCK_STREAM;
+	server.ai_flags = AI_PASSIVE;
+	
+	int res =  getaddrinfo(NULL, argv[1], &server, &results);
+	if(res!=0){
+		printf("Error: %s\n", gai_strerror(res)); exit(2);
+	}
+
+
+	//setup socket
+	int sockfd;
+	if( (sockfd = socket(results->ai_family, results->ai_socktype, results->ai_protocol)) == -1){
+		printf(ANSI_COLOR_CYAN "Error: %d Message: %s Line#: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__);
+		exit(2);
+	}
+
+	//setup bind
+	if( bind(sockfd, results->ai_addr, results->ai_addrlen) == -1){	
+		printf(ANSI_COLOR_CYAN "Error: %d Message: %s Line#: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__); 
+		exit(2);
+	}
+
+	//call listen
+	if( listen(sockfd, 10) == -1){
+		printf(ANSI_COLOR_CYAN "Error: %d Message: %s Line#: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__); 
+		exit(2);
+	}
+
+	//call accept
+	int fd;
+	if( (fd = accept(sockfd, (results->ai_addr), &(results->ai_addrlen))) == -1){
+		printf(ANSI_COLOR_CYAN "Error: %d Message: %s Line#: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__); 
+		exit(2);	
+	}		
+
+
 	return 0; 
 }
