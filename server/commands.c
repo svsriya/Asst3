55478a059f4123b0f031f9c62bb9a8dee1d4c256
Sriya Vudata <sv520@less.cs.rutgers.edu>
2019-04-24 18:57:57 -0400

    added search for proj method

--- server/commands.c
+++ server/commands.c
@@ -16,5 +16,41 @@
 #define ANSI_COLOR_CYAN    "\x1b[36m"
 #define ANSI_COLOR_RESET   "\x1b[0m"
 
+char* searchProj( char* );
+void create( char* );
+// checks whether project already exists or not
+char* searchProj( char* projname )
+{
+	DIR* dirp;
+	char* projpath;
 
+	projpath = (char*)malloc( 7 + strlen(projname) + 1 );
+	strcpy( projpath, "./root/" );
+	strcat( projpath, projname );
+	projpath[strlen(projpath)] = '\0';
 
+	if( (dirp = opendir(projpath)) == NULL )
+		return NULL;
+	return projpath;
+}
+
+void create( char* projname )
+{
+	char* projpath = searchProj( projname );
+	if( projpath != NULL )
+	{	// EXIT OR RETURN???
+		printf( ANSI_COLOR_RED "Error: project with that name already exists\n" ANSI_COLOR_RESET );
+		return;
+	}
+	// path of project: ./root/name
+	// create the directory for new project
+	// create .Manifest inside it
+	// send .Manifest over to client
+}
+
+int main( int argc, char** argv )
+{
+	char* result = searchProj( argv[1] );
+	printf( "result = %s\n", result );
+	return 0;
+}
