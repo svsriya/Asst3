31818424aa84d1e525bae0073e04b18bc6f0f878
Sriya Vudata <sv520@less.cs.rutgers.edu>
2019-04-27 01:45:18 -0400

    add/remove done\!

--- Makefile
+++ Makefile
@@ -1,10 +1,10 @@
 all: WTF WTFserver
 
-WTF: WTFclient.c
-	gcc -O -g -o WTF WTFclient.c
+WTF: ./client/WTFclient.c
+	gcc -O -lssl -lcrypto -g -o WTF ./client/WTFclient.c
 
-WTFserver: WTFserver.c
-	gcc -O -g -o WTFserver WTFserver.c
+WTFserver: ./server/WTFserver.c
+	gcc -O -g -o WTFserver ./server/WTFserver.c
 
 clean:
 	rm -rf WTF
--- client/clientcommands.c
+++ client/clientcommands.c
@@ -34,6 +34,7 @@ void writeM( char*, Manifest* );
 void printM( Manifest* );
 char* hashcode( char* );
 void addM( char*, char* );
+void removeM( char*, char* );
 void freeManifest( Manifest* );
 
 // returns the  path of the project if it exists, otherwise returns null
@@ -113,6 +114,46 @@ void build( char* manpath, Manifest** head )
 	//free( mbuffer );
 }
 
+void writeM( char* manpath, Manifest* head )
+{
+	// filepath version server removed hash
+	int fd;
+	char* buff;
+	if( (fd = open( manpath, O_TRUNC | O_APPEND | O_WRONLY ) ) == -1 )
+	{
+		printf( ANSI_COLOR_CYAN "Errno: %d Message: %s Line: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__ );
+		return;	//exit function??
+	}
+	
+	Manifest* ptr;
+	for( ptr = head; ptr != NULL; ptr = ptr->next )
+	{
+		int len = strlen(ptr->filepath)+strlen(ptr->vnum)+strlen(ptr->onServer)+strlen(ptr->removed)+strlen(ptr->hash)+5;
+		if( ptr == head ) len += strlen(ptr->projversion)+1;
+		buff = (char*)malloc( len + 1 );
+		buff[0] = '\0';
+		if( ptr == head ){ 
+			strcat( buff, ptr->projversion );
+			strcat( buff, "\n" );
+		}
+		strcat( buff, ptr->filepath );
+		strcat( buff, "\t" );
+		strcat( buff, ptr->vnum );
+		strcat( buff, "\t" );
+		strcat( buff, ptr->onServer );
+		strcat( buff, "\t" );
+		strcat( buff, ptr->removed );
+		strcat( buff, "\t" );
+		strcat( buff, ptr->hash );
+		strcat( buff, "\n" );
+		if( write( fd, buff, strlen(buff) ) == -1 ){
+			printf( ANSI_COLOR_CYAN "Errno: %d Message: %s Line: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__ );
+			return; //???
+		}	
+		free(buff);
+	}
+}
+
 void printM( Manifest* head )
 {
 	Manifest* ptr = head;
@@ -144,11 +185,11 @@ char* hashcode( char* filepath )
                 printf( ANSI_COLOR_CYAN "Errno: %d Message: %s Line: %d\n" ANSI_COLOR_RESET, errno, strerror(errno), __LINE__ );
         }
         hash = SHA256( filebuff, strlen(filebuff),0);
-        
+	 
 	char* result = (char*)malloc( (strlen(hash)*2) + 1);
 	result[0] = '\0';
 	int x;
-	for( x = 0; x < strlen( hash ); x++ )
+	for( x = 0; x < SHA256_DIGEST_LENGTH; x++ )
 		sprintf( result+(x*2), "%02x", hash[x] );
 	result[x*2] = '\0'; 
 	free(filebuff);
@@ -173,15 +214,76 @@ void addM( char* projname, char* filename )
 	strcat( manpath, "/.Manifest" );
 	build( manpath, &head );
 	//check the linked list to see if the entry is already in the manifest
-	Manifest* ptr;
+	Manifest* ptr = head;
 	Manifest* prev;
 	while( ptr != NULL )
 	{
-		if( 
+		if( strcmp( ptr->filepath, filename ) == 0 )
+		{
+			//file already exists so update the hash
+			ptr->hash = hashcode(filename);
+			if( strcmp(ptr->removed, "1") == 0 ) ptr->removed = "0";
+			else printf( ANSI_COLOR_RED "Warning: file already exists in .Manifest, hashcode has been updated\n" ANSI_COLOR_RESET );
+			writeM( manpath, head );
+		//	printM( head );
+			freeManifest( head );
+			return;
+		}  
+		prev = ptr;
+		ptr = ptr->next;
 	}
-
+	// new file!
+	ptr = (Manifest*)malloc( sizeof(Manifest) );
+	ptr->projversion = prev->projversion;
+	ptr->filepath = filename;
+	ptr->vnum = "1.0";
+	ptr->hash = hashcode(filename);
+	ptr->onServer = "0";
+	ptr->removed = "0";
+	ptr->next = NULL;
+	prev->next = ptr;
+	writeM( manpath, head );
+	//printM( head );
 	freeManifest( head );
 	free(manpath);
+	printf( ANSI_COLOR_YELLOW "File has been added to .Manifest\n" ANSI_COLOR_RESET );
+}
+
+void removeM( char* projname, char* filename )
+{
+	//check that project exists on client
+	char* projpath = searchProj( projname );
+	char* manpath;
+	Manifest* head = NULL;
+	if( projpath == NULL )
+	{
+		printf( ANSI_COLOR_RED "Error: project does not locally exist\n" ANSI_COLOR_RESET );
+		return;
+	}
+	printf( ANSI_COLOR_YELLOW "Project locally found!\n" ANSI_COLOR_RESET );
+	manpath = (char*)malloc( strlen(projpath) + strlen("/.Manifest") + 1 );
+	manpath[0] = '\0';
+	strcat( manpath, projpath );
+	strcat( manpath, "/.Manifest" );
+	build( manpath, &head );
+	//check the linked list to see if the entry is already in the manifest
+	Manifest* ptr = head;
+	while( ptr != NULL )
+	{
+		if( strcmp( ptr->filepath, filename ) == 0 )
+		{
+			//file already exists so update the hash
+			ptr->removed = "1";
+			printf( ANSI_COLOR_YELLOW "File has been marked for removal\n" ANSI_COLOR_RESET );
+			writeM( manpath, head );
+		//	printM( head );
+			freeManifest( head );
+			return;
+		}  
+		ptr = ptr->next;
+	}
+	printf( ANSI_COLOR_RED "Error: file does not exist\n" ANSI_COLOR_RESET );
+
 }
 
 void freeManifest( Manifest* head )
@@ -197,10 +299,11 @@ void freeManifest( Manifest* head )
 
 int main( int argc, char** argv )
 {
-	char* filepath = argv[1];
-	Manifest* head = NULL;
-	build( filepath, &head );
-	printM( head );
-	freeManifest( head ); 
+	char* projname = argv[2];
+	char* filepath = argv[3];
+	if( strcmp( argv[1], "add" ) == 0 )
+		addM(projname, filepath);
+	else if( strcmp( argv[1], "remove" ) == 0 )
+		removeM( projname, filepath ); 
 	return 0; 
 }
